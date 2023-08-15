f2e4ced7b4cb31a21cc543a18bdcd9a4e4424dcd
answer666 <wei.lin@jingdigital.com>
2023-08-15 15:21:27 +0800

    $ 没有引入fix

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -11,7 +11,7 @@ return array(
 	//'DB_PREFIX' => 'tongji_', // 数据库表前缀
 	//'DB_CHARSET'=> 'utf8', // 字符集
 	//
-	'DB_TYPE'   => 'mysql', // 数据库类型
+	'DB_TYPE'   => 'mysqli', // 数据库类型
 	'DB_HOST'   => 'tongji-mysql', // 服务器地址
 	'DB_NAME'   => 'tongji', // 数据库名
 	'DB_USER'   => 'root', // 用户名
